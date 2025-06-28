3ea14e4196a5bb5f36496d0e8274bd1a06c3d561
lowllyy <niilltee555@gmail.com>
2025-06-28 21:58:54 +0300

    Add JavaScript code to resize the square

---
+++ js-tasks/task-1/main.js
@@ -0,0 +1,32 @@
+const input_w = document.getElementById('width-setting');
+const input_h = document.getElementById('height-setting');
+const square = document.getElementById('box');
+
+function update_size (){
+    let value_w = input_w.value;
+    let value_h = input_h.value;
+
+    let value_w_numb = Number(value_w);
+    let value_h_numb = Number(value_h);
+
+    // проверка на числовое заполнение полей неотрицательными значениями
+
+    if (isNaN(value_w_numb) || value_w_numb<0){
+        value_w = "100";
+        input_w.value = value_w;
+    }
+    if (isNaN(value_h_numb) || value_h_numb<0){
+        value_h = "100";
+        input_h.value = value_h;
+    }
+
+    // изменение размеров фигуры
+
+    square.style.width = value_w + 'px';
+    square.style.height = value_h + 'px';
+};
+
+// обработка события изменения текстового значения input
+
+input_w.oninput = update_size;
+input_h.oninput = update_size;
